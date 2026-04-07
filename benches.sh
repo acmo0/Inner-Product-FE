dab9607c114313d2c534802cf2d5901e245544ab
acmo0 <gregoire.fremion@telecom-sudparis.eu>
2026-04-07 13:29:55 +0200

    Add script to automate benches

---
+++ benches.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -euo pipefail
+IFS=$'\n\t'
+
+function write_len {
+	sed -i "s/= .*;/= ${$1};/g" fe/src/consts.rs
+}
+
+
+for len in "16 32 64 128 256 512"; do
+	write_len $len
+	cargo clean
+	RUSTFLAGS="-C target-cpu=native" cargo +nightly build --release
+	read -n 1 -p "Continue ?"
+done
